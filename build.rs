f69b3ab6a1b8aae6fb3c8a33017bc24c8be02a57
Trevor Bentley <trevor@trevorbentley.com>
2019-06-23 20:56:55 +0200

    Add alternative search path for private frameworks

--- build.rs
+++ build.rs
@@ -8,10 +8,17 @@ fn add_xcode_private_framework_path() {
         .expect("Failed to run xcode-select");
     let mut xcode_dir = String::from_utf8(xcode_dir.stdout).unwrap();
     xcode_dir.pop(); // remove trailing newline
-    let framework_dir = format!("{}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks/",
-                                xcode_dir);
-    println!("XCode PrivateFramework dir: {}", framework_dir);
 
+    let private_framework_dirs = vec![
+        format!("{}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks/",
+                xcode_dir),
+        "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/PrivateFrameworks/DFRFoundation.framework/".into(),
+    ];
+    let framework_dir = private_framework_dirs.iter().filter(|dir| {
+        std::path::Path::new(dir).is_dir()
+    }).next().expect("XCode PrivateFramework directory not found.");
+
+    println!("XCode PrivateFramework dir: {}", framework_dir);
     println!("cargo:rustc-link-search=framework={}", framework_dir);
 }
 
